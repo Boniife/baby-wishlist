7d3c002ead04fd846d5837222f648ec1733b1b78
Boniife <anyanwuifeanyi3@gmail.com>
2024-05-15 15:36:09 +0100

    updates

--- app/Navbar/page.tsx
+++ app/Navbar/page.tsx
@@ -1,14 +1,17 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import { FiArrowUp, FiShoppingCart } from 'react-icons/fi';
 import Link from 'next/link';
 
 const Navbar = () => {
   const [showScroll, setShowScroll] = useState(false);
   const [showCart, setShowCart] = useState(false); 
+  const [isMobile, setIsMobile] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
 
-  const scrollToSection = (sectionId: any) => {
+  const scrollToSection: any = (sectionId: any) => {
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
@@ -27,6 +30,10 @@ const Navbar = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   useEffect(() => {
     window.addEventListener('scroll', checkScrollTop);
     return () => {
@@ -34,28 +41,66 @@ const Navbar = () => {
     };
   }, [showScroll]);
 
+  const handleResize = () => {
+    if (window.innerWidth <= 629) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className='navbar'>
       <div className='navbar_wrap'>
-        <div className='wrapper'>
-          <div className='wrapper_img-wrap'>
-            <Image 
-              width={100}
-              height={100}
-              src={'/images/baby2.png'}
-              alt=''
-            />
-          </div>
-          
-          <div className='wrapper_text-wrap cursor-pointer'>
-            <p className='list1' onClick={() => scrollToSection('dresses-section')}>Dresses</p>
-            <p className='list2' onClick={() => scrollToSection('footwears-section')}>Footwears</p>
-            <p className='list1' onClick={() => scrollToSection('accessories-section')}>Accessories</p>
-            <p className='list2' onClick={() => scrollToSection('toys-section')}>Toys</p>
-          </div>
+          {isMobile && (
+      <div className="mobile-header text-white">
+        <div className="toggle-icon" onClick={toggleSidebar}>
+          {showSidebar ? <FaTimes /> : <FaBars />}
+        </div>
+        <Image 
+          width={100}
+          height={100}
+          src={'/images/baby2.png'}
+          alt=''
+          className="logo-image"
+        />
+        <div className={`sidebar ${showSidebar ? 'active' : ''}`}>
+          <p className='list1' onClick={() => scrollToSection('dresses-section')}>Dresses</p>
+          <p className='list2' onClick={() => scrollToSection('footwears-section')}>Footwears</p>
+          <p className='list1' onClick={() => scrollToSection('accessories-section')}>Accessories</p>
+          <p className='list2' onClick={() => scrollToSection('toys-section')}>Toys</p>
         </div>
       </div>
-      {/* Cart icon */}
+    )}
+
+        {!isMobile && (
+          <div className='wrapper'>
+            <div className='wrapper_img-wrap'>
+              <Image 
+                width={100}
+                height={100}
+                src={'/images/baby2.png'}
+                alt=''
+              />
+            </div>
+            
+            <div className='wrapper_text-wrap cursor-pointer'>
+              <p className='list1' onClick={() => scrollToSection('dresses-section')}>Dresses</p>
+              <p className='list2' onClick={() => scrollToSection('footwears-section')}>Footwears</p>
+              <p className='list1' onClick={() => scrollToSection('accessories-section')}>Accessories</p>
+              <p className='list2' onClick={() => scrollToSection('toys-section')}>Toys</p>
+            </div>
+          </div>
+        )}
+      </div>
       <div className="fixed bottom-4 right-4 z-50">
         <Link href="/cart"> 
             <FiShoppingCart className="bg-pink-400 p-1 rounded-full text-4xl hover:bg-gray-600 cursor-pointer" />
@@ -73,5 +118,6 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
 
